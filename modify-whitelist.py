c02d5970e7f124ffd020723e7f7dc014eab3ee57
Dan Sikes <Daniel.Sikes@ge.com>
2019-03-05 23:11:39 -0500

    testing

--- modify-whitelist.py
+++ modify-whitelist.py
@@ -7,10 +7,21 @@ ansible_workspace_dir = "%s/ansible-code" % (os.getcwd())
 def get_yaml_content(f):
     with open(f, 'r') as stream:
         try:
-            print(yaml.load(stream))
+            return yaml.load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
+def set_yaml_content(f, d):
+    with open('result.yml', 'w') as stream:
+        try:
+            yaml.dump(d, stream, default_flow_style=False)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-yc = get_yaml_content("%s/%s" % (ansible_workspace_dir, 'wordpress-nginx/site.yml'))
+f = "%s/%s" % (ansible_workspace_dir, 'wordpress-nginx/site.yml')
+
+yc = get_yaml_content(f)
 print(yc)
+yc["newkey"] = "newval"
+
+sc = set_yaml_content(f, yc)

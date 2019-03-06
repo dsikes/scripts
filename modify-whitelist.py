6838ae6e9f341ce1a3b91633bc7242a5395341ff
Dan Sikes <Daniel.Sikes@ge.com>
2019-03-05 23:00:28 -0500

    test

--- modify-whitelist.py
+++ modify-whitelist.py
@@ -1,3 +1,16 @@
 import os
-# os.chdir('../')
-print(os.listdir(os.getcwd()))
+# import fileinput
+import yaml
+ansible_workspace_dir = "%s/ansible-code" % (os.getcwd())
+
+
+def get_yaml_content(f):
+    with open(f, 'r') as stream:
+        try:
+            print(yaml.load(stream))
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+yc = get_yaml_content("%s/%s" % (ansible_workspace_dir, 'wordpress-nginx/site.yml'))
+print(yc)

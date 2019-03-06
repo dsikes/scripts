88587fd528be267c6b3ad3459bb91b93f4008ed2
Dan Sikes <Daniel.Sikes@ge.com>
2019-03-06 10:59:48 -0500

    test

--- whitelist-request-parser.py
+++ whitelist-request-parser.py
@@ -1,12 +1,10 @@
-import yaml
+import sys
+import json
 
-def get_yaml_content(f):
-    with open(f, 'r') as stream:
-        try:
-            return yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+try:
+    wlr = json.loads(sys.argv[1])
+except Exception as err:
+    print(err)
+    sys.exit(1)
 
-whitelist = get_yaml_content("/wlr/wlr.yaml")
-
-print(whitelist)
+print(wlr)
